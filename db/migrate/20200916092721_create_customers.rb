18c53f29cb100e9510b8b69065919c1404e6b373
vagrant <vagrant@localhost.localdomain>
2020-09-17 01:43:12 +0000

    column-edit

--- db/migrate/20200916092721_create_customers.rb
+++ db/migrate/20200916092721_create_customers.rb
@@ -1,7 +1,16 @@
 class CreateCustomers < ActiveRecord::Migration[5.2]
   def change
     create_table :customers do |t|
-
+      t.string :kanji_familyname
+      t.string :kanji_firstname
+      t.string :kana_familyname
+      t.string :kana_firstname
+      t.string :postal_code
+      t.string :address
+      t.string :telephone_number
+      t.string :email
+      t.string :encrypted_password
+      t.boolean :is_deleted, null: false, default: false
       t.timestamps
     end
   end
--- db/migrate/20200916092759_create_products.rb
+++ db/migrate/20200916092759_create_products.rb
@@ -1,7 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.references :genre_id, foreign_key: true, null: false
+      t.string :name, index: true
+      t.text :explanation
+      t.string :image_id
+      t.integer :price
+      t.boolean :is_active, null: true
       t.timestamps
     end
   end
