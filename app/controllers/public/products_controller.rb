d855ea45860aed5b6553fad79c88af41c0657316
vagrant <vagrant@localhost.localdomain>
2020-09-19 13:29:12 +0000

    商品一覧ページでçほぼできた

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,8 +1,15 @@
 class Public::ProductsController < ApplicationController
 
 	def index
-		@product = Product.new
-		@products = Product.all
+		if params[:genre_id].present?
+			@genre = Genre.where(params[:genre_id])
+			@products = Product.where(genre_id: params[:genre_id])
+			@name = @genre.name
+		else 
+			@genre = Genre.new
+			@name = "商品"
+			@products = Product.all
+		end
 	end
 
 	def show
