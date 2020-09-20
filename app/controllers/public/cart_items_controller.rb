9dfe1ea3c2bef56f6f3d9d91a0d81f9a43fe64f3
vagrant <vagrant@localhost.localdomain>
2020-09-20 13:22:10 +0000

    showページ完成

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,14 +1,15 @@
 class Public::CartItemsController < ApplicationController
 
 	def create
-		@cart_item = current_customer.cart_items.build(cart_item_params)
+		@cart_item = CartItem.new(cart_item_params)
+		@cart_item.customer_id = current_customer.id
 		@current_item = CartItem.find_by(product_id: @cart_item.product_id,customer_id: @cart_item.customer_id)
 		if @current_item.nil?
 		  if @cart_item.save
 			flash[:success] = 'カートに商品が追加されました！'
 			redirect_to cart_items_path
 		  else
-			@carts_items = @customer.cart_items.all
+			@carts_items = set_customer.cart_items.all
 			render 'index'
 			flash[:danger] = 'カートに商品を追加できませんでした。'
 		  end
