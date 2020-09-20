9de9e0a2d94e54b7e550a0edd8c417c5d14c2cb9
vagrant <goodmountain37@gmail.com>
2020-09-20 02:31:28 +0000

    [merge] feature-customer-page

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,2 +1,20 @@
 class Admins::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  private 
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
