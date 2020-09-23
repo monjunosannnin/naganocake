328b04ff4df1394a2d8aef34ad3c32625133dd3b
vagrant <vagrant@localhost.localdomain>
2020-09-23 16:37:34 +0000

    customerpage

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,36 +1,36 @@
 class Public::CustomersController < ApplicationController
-  # before_action :authenticate_customer!
+  before_action :authenticate_customer!
 
   def show
-    @customer = Customer.find(customer_params)
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
-    @customer = Customer.find(customer_params)
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
+    @customer = Customer.find(current_customer.id)
     if @customer.update(customer_params)
        flash[:notice] = 'It was successfully updated.'
-       redirect_to mypege_path
+       redirect_to mypage_path
     else
        render :edit
-       @customer = Customer.find(customer_params)
-  end
+    end
   end
 
   def withdrawal
   end
 
   def unsubscribe
+    @customer = Customer.find(current_customer.id)
     @customer.update(is_deleted: true)
     reset_session
-    flash[:notice] = "ご利用ありがとうございました。またのお越しをお待ちしております。"
     redirect_to root_path
   end
 
   private
     def customer_params
-      params.require(:customer).permit(:kanji_familyname, :kanji_firstname, :kana_familyname, :kana_firstname, :email, :postal_code, :address, :telephone_number, :is_deleted)
+      params.require(:customer).permit(:kanji_familyname, :kanji_firstname, :kana_familyname, :kana_firstname, :postal_code, :address, :telephone_number, :email, :is_deleted)
     end
   end
