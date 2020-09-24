77f411ceb979420ab8f03c2881c3d54c6eee832a
vagrant <vagrant@localhost.localdomain>
2020-09-24 07:11:02 +0000

    add-validates

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :kanji_familyname, :kanji_firstname, :kana_familyname, :kana_firstname, :postal_code, :address, :telephone_number, :email, :encrypted_password, presence: true
   
   has_many :orders, foreign_key: 'customer_id', dependent: :destroy
   has_many :cart_items, dependent: :destroy
