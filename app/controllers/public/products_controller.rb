688022b7b146e5a225447b2f7e6feba00cb422ac
vagrant <vagrant@localhost.localdomain>
2020-09-26 07:54:37 +0000

    注文機能修正

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,14 +1,15 @@
 class Public::ProductsController < ApplicationController
 
+
 	def index
 		if params[:genre_id].present?
 			@genre = Genre.find(params[:genre_id])
-			@products = Product.where(genre_id: params[:genre_id])
+			@products = Product.where(genre_id: params[:genre_id]).page(params[:page]).per(8)
 			@name = @genre.name
 		else
 			@genre = Genre.new
 			@name = "商品"
-			@products = Product.all
+			@products = Product.all.page(params[:page]).per(8)
 		end
 		@genres = Genre.all
 	end
@@ -17,6 +18,12 @@ class Public::ProductsController < ApplicationController
 		@genres = Genre.all
 		@product = Product.find(params[:id])
 		@cart_item = CartItem.new(params[:product_id])
+	end
+
+	private
+
+	def ensure_genres
+	  @genres = Genre.is_valid
 	end
 
 end
